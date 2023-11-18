d00d0dd82fcb1e8a03c2964c62348c99376dfb14
nickwkt <nickb55245@gmail.com>
2023-11-18 20:07:29 +0800

    Expose customer flights api endpoint and health check

--- app/application.py
+++ app/application.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from customer_api.customers import Customer
 
 app = FastAPI()
 
@@ -19,6 +20,18 @@ facilities = {
         Facility(name="Hangar 2", type="hangar")
     ]
 }
+
+@app.get("/")
+async def health_check():
+    return {"message": "Customer Info API is running"}
+
+
+@app.get("/v1/customer/{pid}")
+async def customer_flights(pid: str):
+    customer = Customer(pid)
+    return customer.get_flights()
+
+
 
 @app.get("/v1/facilities/{airport}")
 def get_facilities(airport: str, type: str):
